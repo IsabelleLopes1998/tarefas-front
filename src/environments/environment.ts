891434e05ddf9ccb844d8911afdf11119be72bf5
Isabelle Lopes <isabelle.biomed@gmail.com>
2025-05-25 18:31:03 -0300

    adicionei rota local e vm back

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,8 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://54.166.83.75:8080',
+  apiBaseUrl: 'http://localhost:8080',
+  /* apiBaseUrl: 'http://54.166.83.75:8080' */
 
 };
 
