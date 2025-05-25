55b42d38b3b4c1b090494c43ad58e106033faf2a
Isabelle Lopes <isabelle.biomed@gmail.com>
2025-05-25 18:03:47 -0300

    feat: modificacao do apiBaseUrl para receber a rota do back

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,7 @@
 
 export const environment = {
   production: false,
-  // apiBaseUrl: 'https://somelicontabilidadeback.onrender.com'
-  apiBaseUrl: 'http://localhost:8080',
+  apiBaseUrl: 'http://54.166.83.75:8080',
 
 };
 
